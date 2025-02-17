5d162b334f0d4c83c6ebf1b438eb84689f5aef61
Emine <emine.kurucu.7@gmail.com>
2025-02-17 14:27:31 +0300

    dosya islemleri eklendi

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,72 @@
+import java.io.*;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //createFile();
+        getFileInfo(); // File Name: students.txt
+        readFile();
+        fileWrite();
+        readFile();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    }
+
+    public static void createFile(){
+        File file = new File("C:\\javademos\\files\\students.txt");
+        try {
+            if(file.createNewFile()){ // yeni file ekledik ama try catch olmalı/ Oluşturu true,false döner
+                System.out.println("Dosya olusturuldu.");
+            } else {
+                System.out.println("Dosya zaten mevcut.");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    public static void getFileInfo(){
+        File file = new File("C:\\javademos\\files\\students.txt");
+        file.exists(); // ilgili dizinde dosya varsa true döndürür
+        if (file.exists()){
+            System.out.println("File Name: "+ file.getName());
+            System.out.println("Dosya Yolu: "+ file.getAbsolutePath());// Dosya Yolu: C:\javademos\files\students.txt
+            System.out.println("Dosya yazilabilir mi: "+ file.canWrite());
+            System.out.println("Dosya okunabilir mi: "+ file.canRead());
+            System.out.println("Dosya boyutu(byte): "+ file.length());
+        }
+    }
+
+    public static void readFile(){
+        File file = new File("C:\\javademos\\files\\students.txt");
+        // dosyada okuma yapabilmek için Scanner'dan faydalanılır
+        try {
+            Scanner reader = new Scanner(file);
+            while (reader.hasNextLine()){ // okunacak satır varsa anlamında
+                String line = reader.nextLine();
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void fileWrite(){
+        try {
+            BufferedWriter writer =new BufferedWriter(new FileWriter("C:\\javademos\\files\\students.txt", true));
+            // "C:\\javademos\\files\\students.txt" bu haldeyken dosyanın üzerine ayzar önceki yazilanlar gider
+            writer.write("\nDiger satirlar\n");
+            writer.write("Bir satir daha");
+            System.out.println("Dosyaya yazildi.");
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        /* writer.newLine()
+        writer.write() yazarsak önce yeni satır oluşturur sonra yazar
+         */
+
     }
 }
